1914d848e9f1806f07d309a5c0f33767412c8c88
davidjrb <davidjberman@gmail.com>
2024-09-09 16:33:54 +0000

    flask-tables to broadcast externally

--- flask-tables.py
+++ flask-tables.py
@@ -79,4 +79,4 @@ def show_table(table_name):
         return render_template_string(HTML_TEMPLATE, rows=rows, columns=columns, table_name=table_name)
 
 if __name__ == '__main__':
-    app.run(debug=True, port=3500)
+    app.run(host='0.0.0.0', debug=True, port=3500)
